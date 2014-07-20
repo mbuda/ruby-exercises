cdae6f8dd5f0b94fefbb24b9cfb836e62fe266bd
Marta Buda <bmarta1991@o2.pl>
2014-07-20 11:08:43 +0200

    hash_test all pass

--- core-types/hash_test.rb
+++ core-types/hash_test.rb
@@ -8,93 +8,99 @@ class HashTest < Minitest::Homework
 
   def test_empty
     boolean = {}.empty?
-    maybe boolean
+    assert boolean
     boolean = {"a" => "apple"}.empty?
-    maybe boolean
+    refute boolean
   end
 
   def test_access
     ages = {16 => ["Alice", "Bob"], 24 => ["Charlie", "Dave"], 37 => ["Eve"]}
-    assert_equal __, ages[16]
-    assert_equal __, ages[37]
+    assert_equal ["Alice", "Bob"], ages[16]
+    assert_equal ["Eve"], ages[37]
   end
 
   def test_assignment
     stuff = {"a" => ["aardvark", "apple"], "b" => ["butcher", "ballerina"]}
     stuff["c"] = ["cook", "caramel candy"]
-    assert_equal __, stuff["c"]
+    assert_equal ["cook", "caramel candy"], stuff["c"]
     stuff["b"] = "brandy"
-    assert_equal __, stuff["b"]
+    assert_equal "brandy", stuff["b"]
     stuff["a"] = stuff["a"] + ["apricot"]
-    assert_equal __, stuff["a"]
+    assert_equal ["aardvark", "apple", "apricot"], stuff["a"]
   end
 
   def test_keys
-    assert_equal __, {}.keys
-    assert_equal __, {1 => "one", 2 => "two"}.keys
-    assert_equal __, {"a" => "apple", "banana" => "b"}.keys
+    assert_equal [], {}.keys
+    assert_equal [1, 2], {1 => "one", 2 => "two"}.keys
+    assert_equal ["a", "banana"], {"a" => "apple", "banana" => "b"}.keys
   end
 
   def test_values
-    assert_equal __, {}.values
-    assert_equal __, {1 => "one", 2 => "two"}.values
-    assert_equal __, {"a" => "apple", "banana" => "b"}.values
+    assert_equal [], {}.values
+    assert_equal ["one", "two"], {1 => "one", 2 => "two"}.values
+    assert_equal ["apple", "b"], {"a" => "apple", "banana" => "b"}.values
   end
 
   def test_include
+    #checking only keys
     boolean = {}.include?("a")
-    maybe boolean
+    refute boolean
     boolean = {"a" => "apple"}.include?("a")
-    maybe boolean
+    assert boolean
     boolean = {"a" => "apple"}.include?("apple")
-    maybe boolean
+    refute boolean
   end
 
   def test_member
+    #checking only keys ... again
+    #the same as #key?, #has_key?
     boolean = {}.member?("a")
-    maybe boolean
+    refute boolean
     boolean = {"a" => "apple"}.member?("a")
-    maybe boolean
+    assert boolean
     boolean = {"a" => "apple"}.member?("apple")
-    maybe boolean
+    refute boolean
   end
 
   def test_invert
-    assert_equal __, {"a" => "apple", "b" => "banana"}.invert
-    assert_equal __, {1 => "one", 2 => "two"}.invert
+    #key -> value, value -> key inversion
+    assert_equal ({"apple" => "a", "banana" => "b"}), ({"a" => "apple", "b" => "banana"}.invert)
+    assert_equal ({"one" => 1, "two" => 2}), ({1 => "one", 2 => "two"}.invert)
   end
 
   def test_length
-    assert_equal __, {}.length
-    assert_equal __, {"a" => "apple"}.length
-    assert_equal __, {"a" => "apple", "b" => "banana"}.length
+    assert_equal 0, {}.length
+    assert_equal 1, {"a" => "apple"}.length
+    assert_equal 2, {"a" => "apple", "b" => "banana"}.length
   end
 
   def test_merge
+    #when merge with different value with existing key override old one
     h1 = {}.merge({})
     h2 = {}.merge({"a" => "apple"})
     h3 = {"a" => "apple"}.merge({"b" => "banana"})
     h4 = {"a" => "apple"}.merge({"a" => "avocado", "b" => "banana"})
     h5 = {"a" => "apple", "b" => "banana"}.merge({"b" => "breadfruit"})
 
-    assert_equal __, h1
-    assert_equal __, h2
-    assert_equal __, h3
-    assert_equal __, h4
-    assert_equal __, h5
+    assert_equal ({}), h1
+    assert_equal ({"a" => "apple"}), h2
+    assert_equal ({"a" => "apple", "b" => "banana"}), h3
+    assert_equal ({"a" => "avocado", "b" => "banana"}), h4
+    assert_equal ({"a" => "apple", "b" => "breadfruit" }), h5
   end
 
   def test_update
+    #the same as merge, just different name
     h1 = {}.update({})
     h2 = {}.update({"a" => "apple"})
     h3 = {"a" => "apple"}.update({"b" => "banana"})
     h4 = {"a" => "apple"}.update({"a" => "avocado", "b" => "banana"})
     h5 = {"a" => "apple", "b" => "banana"}.update({"b" => "breadfruit"})
 
-    assert_equal __, h1
-    assert_equal __, h2
-    assert_equal __, h3
-    assert_equal __, h4
-    assert_equal __, h5
+    assert_equal ({}), h1
+    assert_equal ({"a" => "apple"}), h2
+    assert_equal ({"a" => "apple", "b" => "banana"}), h3
+    assert_equal ({"a" => "avocado", "b" => "banana"}), h4
+    assert_equal ({"a" => "apple", "b" => "breadfruit"}), h5
   end
 end
